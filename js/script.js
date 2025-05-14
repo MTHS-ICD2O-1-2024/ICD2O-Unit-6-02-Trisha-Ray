7214ac1ee24dbee4ceefc7ff43f529e5aced42f3
default <trisha.ray@stu.ocsb.ca>
2025-05-14 17:17:56 +0000

    template

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Trisha Ray
+// Created on: Feb 2025
 // This file contains the JS functions for index.html
+'use strict'
+
+function calculate() {
+
+ // input
+  let firstNumber = parseFloat(document.getElementById("numberone").value)
+  const secondNumber = parseFloat(document.getElementById("numbertwo").value)
+  let answer = 0
+
+  // process
+  while (firstNumber >= 1) {
+    answer = answer + secondNumber
+  firstNumber = firstNumber - 1;
+  }
+  // output
+  document.getElementById("results").innerHTML =
+  'The number is: ' + answer
+}
