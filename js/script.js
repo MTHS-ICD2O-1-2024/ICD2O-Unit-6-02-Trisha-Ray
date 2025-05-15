63bd165fde9522f710207bb308eebf063b6c2883
default <trisha.ray@stu.ocsb.ca>
2025-05-15 16:52:40 +0000

    cookie clicker works

--- js/script.js
+++ js/script.js
@@ -3,21 +3,23 @@
 // Created by: Trisha Ray
 // Created on: Feb 2025
 // This file contains the JS functions for index.html
+
 'use strict'
 
-function calculate() {
-
- // input
-  let firstNumber = parseFloat(document.getElementById("numberone").value)
-  const secondNumber = parseFloat(document.getElementById("numbertwo").value)
-  let answer = 0
-
-  // process
-  while (firstNumber >= 1) {
-    answer = answer + secondNumber
-  firstNumber = firstNumber - 1;
+function saveCookieAmount () {
+  let savedCount = localStorage.getItem('cookieCount')
+  if (savedCount === null) {
+    savedCount = 0
   }
-  // output
-  document.getElementById("results").innerHTML =
-  'The number is: ' + answer
+  document.getElementById('cookieCount').textContent = savedCount
+}
+
+function incrementCookieClicked () {
+  let savedCount = localStorage.getItem('cookieCount')
+  if (savedCount === null) {
+    savedCount = 0
+  }
+  const newCount = parseInt(savedCount) + 1
+  localStorage.setItem('cookieCount', newCount)
+  document.getElementById('cookieCount').textContent = newCount
 }
